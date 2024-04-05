54291950b6d4c7f41a662b2f272a379f7cdd809d
Cateven98 <80330455+Cateven98@users.noreply.github.com>
2024-04-05 17:08:18 +0200

    Update indovina_numero.py

--- indovina_numero.py
+++ indovina_numero.py
@@ -1,7 +1,3 @@
-#ESERCIZIO BASE:
-
-#non volgio che mi dia voi giocare ancora? subito, lo voglio solo dopo che ha indovinato il numero
-
 #funzione per randomizzare:
 import random
 def randomizzatore():
@@ -9,22 +5,21 @@ def randomizzatore():
 
 #numero segreto: 
 numero_segreto=randomizzatore() 
-print("il nunero da indovinare è",numero_segreto)
+print("il numero da indovinare è", numero_segreto)
 
 tentativi=1 #inzializzo i tentativi
 while tentativi==True:
-    gioco=input("vuoi giocare ancora? ")
+    gioco=input("vuoi giocare? ")
     while gioco=="si":
         numero=int(input("dammi un numero: "))
         if numero_segreto>numero:
             print("il numero è troppo piccolo")
-            pass
         elif numero_segreto<numero:
             print("il numero è troppo grande")
-            pass
-        else: 
+        else:
             print("hai indovinato!")
-            break
+            gioco=input("vuoi giocare ancora? ")
+            #break
     if gioco=="no":
         tentativi==False
         print("esci dal gioco")
