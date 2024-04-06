ff6e61d5c8edcbebcde979a63f85f2dfba14ff38
Cateven98 <80330455+Cateven98@users.noreply.github.com>
2024-04-06 13:21:58 +0200

    Add files via upload

---
+++ esercizio_gruppo.py
@@ -0,0 +1,46 @@
+#ESERCIZIO 1:
+
+
+#definisco le funzioni: 
+def area_triangolo(b,h):
+    return (b*h)/2
+    
+    
+def area_quadrato(l):
+    return l**2
+
+def area_rettangolo(b1,h1):
+    return b1*h1
+ 
+
+aree=[] #inizializzo lista aree
+
+while True:
+        tipo_area=input("quale area vuoi calcolare? se vuoi far finire il gioco scrivi fine ")
+        if tipo_area=="fine":
+            print("ciao")
+            break
+        elif tipo_area=="triangolo":
+            b=int(input("scrvi misura base del trangolo: "))
+            h=int(input("scrivi misura altezza triangolo: "))
+            triangolo=area_triangolo(b,h)
+            print(triangolo)
+            aree.append(triangolo)
+            print(aree)
+        elif tipo_area=="quadrato":
+            l=int(input("scrivimi la misura del lato del quadrato"))
+            quadrato=area_quadrato(l)
+            print(quadrato)
+            aree.append(quadrato)
+            print(aree)
+        elif tipo_area=="rettangolo":
+            h1=int(input("scrivi altezza rettangolo: "))
+            b1=int(input("scrivi base rettangolo: "))
+            rettangolo=area_rettangolo(b1,h1)  
+            print(rettangolo)
+            aree.append(rettangolo)
+            print(aree)
+
+            
+
+
