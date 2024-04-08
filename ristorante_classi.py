57b22f724e7856ceeff019843517be9462f92d01
Cateven98 <80330455+Cateven98@users.noreply.github.com>
2024-04-08 17:07:22 +0200

    Update ristorante_classi.py

--- ristorante_classi.py
+++ ristorante_classi.py
@@ -40,25 +40,18 @@ menu={
 #7)stampa_menu()
     print(Ristorante.menu)
 '''
-
-
-   
-
 class Ristorante: 
     aperto=False
-    
-    menu={
-    }
+    menu={}
 
     def __init__(self):
         nome=input("insersci nome ristorante: ")
         tipo_cucina=input("inserisci tipo cucina: ")
         self.nome=nome
         self.tipo_cucina=tipo_cucina
-    
 
     def descrivi_ristorante(self):
-        return print("ilristorante", self.nome, "che ha una cucina", self.tipo_cucina)
+        return print("il nome del ristorante è", self.nome, "che fa cucina", self.tipo_cucina)
     
     def stato_apertura():
         if Ristorante.aperto=="False": 
@@ -74,27 +67,68 @@ class Ristorante:
     def chiudi_ristorante(apri): 
        #apri=input("Vuoi sapere se il ristorante è aperto? si/no ")
         Ristorante.aperto==False
-        return print("il ristorante sta chiudendo")
+        print("il ristorante sta chiudendo")
     
 
-    def aggiungi_menu(piatto, prezzo_piatto): 
+    def aggiungi_menu(self): 
         aggiungi=("vuoi aggiungere un piatto e il suo prezzo? si/no")
+        piatto1=input("che piatto vuoi aggiungere? ")
+        prezzo_piatto1=int(input("che prezzo h ail piatto: "))
         if aggiungi=="no":
-            return print("ok, ciao")
+            print("ok, ciao")
         else:
-            piatto=input("che piatto vuoi aggiungere? ")
-            prezzo_piatto=input("che prezzo h ail piatto: ")
-            Ristorante.menu[piatto]= {"prezzo": prezzo_piatto}
-            print("il ristorante aggiunto è", piatto, "che costa", prezzo_piatto)
+            Ristorante.menu[piatto1]= prezzo_piatto1
+            print("il ristorante aggiunto è", piatto1 , "che costa", prezzo_piatto1)
 
 
+
+    def togli_menu(self): 
+        piatto_tolto=input("inserisci il nome del piatto da togliere: ")
+        if piatto_tolto not in Ristorante.menu: 
+            print("il piatto selezionato non è presente nel menu")
+        else: 
+            del Ristorante.menu[piatto_tolto]
+            print("il piatto tolto dal menu è:", piatto_tolto)
+
+
+#prove dei metodi: 
+
+'''
 risto1=Ristorante()
 
 risto1.descrivi_ristorante()
 
 risto1.apri_ristorante()
 
-risto1.togli_menu()
+risto1.chiudi_ristorante()
+
+risto1.aggiungi_menu()
+
+risto1.togli_menu()'''
+
+
+while True:
+    utente=input("Ciao ristoratore, benvenuto nel sito, inserisci il tuo nome utente o se vuoi uscire scrivi esci: ")
+    if utente.lower=="esci" or "0":
+        break
+
+    risto1=Ristorante()
+    risto1.descrivi_ristorante()
+
+    while True: 
+        vedi=input("vuoi sapere se il ristorante è chiuso?")
+
+
+
+   
+
+
+    
+    
+       
+
+
+
 
 
 
