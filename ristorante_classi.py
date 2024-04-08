98e90563285a4c7e27023b93e6651eb2d5a24e42
Cateven98 <80330455+Cateven98@users.noreply.github.com>
2024-04-08 14:47:08 +0200

    Add files via upload

---
+++ ristorante_classi.py
@@ -0,0 +1,24 @@
+'''
+classe Ristorante: 
+aperto=False
+    def __init__(self, nome, tipo_cucina):
+    nome=input("insersci nome ristorante)
+    tipo=input("inserisci tipo cucina)
+    
+    #aggiungo gli attributi a self
+
+
+#aggiungo i metodi: 
+#1) descrivi_ristorante(self)
+    faccio un print, con self.nome e self.cucina
+
+#2) stato apertura()
+    aperto=input("vuoi sapere se il ristorante è aperto?)
+    Ristorante.
+
+#3) apri_ristorante()
+#4) chiudi_ristorante()
+#5) aggiungi il menu()
+#6) togli dal menu()
+#7)stampa_menu()
+'''
