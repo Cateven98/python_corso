b328b4a576296c862016fb67cead5df84f13604d
Cateven98 <80330455+Cateven98@users.noreply.github.com>
2024-04-08 14:51:27 +0200

    Update ristorante_classi.py

--- ristorante_classi.py
+++ ristorante_classi.py
@@ -1,6 +1,11 @@
 '''
 classe Ristorante: 
 aperto=False
+dizionario={
+
+
+}
+
     def __init__(self, nome, tipo_cucina):
     nome=input("insersci nome ristorante)
     tipo=input("inserisci tipo cucina)
@@ -10,13 +15,17 @@ aperto=False
 
 #aggiungo i metodi: 
 #1) descrivi_ristorante(self)
-    faccio un print, con self.nome e self.cucina
+    faccio un print, con self.nome e self.tipo
 
 #2) stato apertura()
     aperto=input("vuoi sapere se il ristorante è aperto?)
-    Ristorante.
+    if Ristorante.aperto==False: 
+    print("il risto è chiuso, torna più tardi)
 
 #3) apri_ristorante()
+    Ristorante.aperto=True
+    if Ristorante.aperto==True: 
+    print("il ristorante sta aprendo, puoi entrare)
 #4) chiudi_ristorante()
 #5) aggiungi il menu()
 #6) togli dal menu()
