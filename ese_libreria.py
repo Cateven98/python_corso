d3d9e84af9c0d757c5889466ebeebc5f77be1e75
Cateven98 <80330455+Cateven98@users.noreply.github.com>
2024-04-09 16:37:41 +0200

    Update ese_libreria.py

--- ese_libreria.py
+++ ese_libreria.py
@@ -5,15 +5,18 @@ class Libro:
         self.autore=autore
         self.isbn=isbn
     
-    def descrizione(self): 
+    def descrizione(self):                                                                           #descrizione libro 
         print(f"titotlo: {self.titolo}, autore; {self.autore}, codice identificativo: {self.isbn}")
 
+
+#classe figlia: 
+
 class Libreria(Libro):
-    def __init__(self, titolo, autore, isbn, catalogo):
+    def __init__(self, titolo, autore, isbn, catalogo):     
         super().__init__(titolo, autore, isbn)
         self.catalogo=catalogo
 
-    def aggiungi_libro(self): 
+    def aggiungi_libro(self):                                 #aggiungo libro alla libreria catalogo
         if self.titolo in catalogo: 
             print("libro già presente in catalogo")
         else: 
@@ -21,14 +24,21 @@ class Libreria(Libro):
             print(f"il libro aggiunto è {self.titolo} dell'autore {self.autore} con codice identificativo {self.isbn}")
         print(catalogo)
     
-    def rimuovi_libro(self):
+    def rimuovi_libro(self):                                #elimino libro alla libreria catalogo, per titolo
         if self.titolo in catalogo:
             del catalogo[self.titolo] 
             print(f"il libro rimosso ha titolo {self.titolo} dell'autore {self.autore} con codice identificativo {self.isbn}")
         else: 
             print("attezione libro non in catalogo")
 
-    
+
+    def cerca_per_titolo(self): 
+        if self.titolo in catalogo: 
+            print(f"")
+            pass
+
+    def mostra_catalogo(self): 
+        print(catalogo)
 
     
 
@@ -36,12 +46,13 @@ class Libreria(Libro):
 
 #prova: 
 
+#definisco il libro: 
 tit=input("inserisci un titolo: ")
 aut=input("insersci un autore: ")
 cod=int(input("insersci un isbn:"))
-catalogo={}
+catalogo={}                          #definisco catalogo come dizionario cosi lo posso andare a comporre
 
-libro1=Libro(tit, aut, cod)
+libro1=Libro(tit, aut, cod)          
 libro1.descrizione()
 
 libro1_piu=Libreria(tit, aut, cod, catalogo)
