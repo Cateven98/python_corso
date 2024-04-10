613fbba384bee82a7baca1b4e5a71f3248339a04
Cateven98 <80330455+Cateven98@users.noreply.github.com>
2024-04-10 10:51:07 +0200

    Add files via upload

---
+++ ese_vendite.py
@@ -0,0 +1,10 @@
+#schema: 
+#inizializzo dizionatio vuoto
+#classe vendite(self, importo_vendita)
+#1) inserimento dati(): input per mettere importi di vendita e converti in interi 
+
+#classe giorno(self, importo vendita, gg)>> classe figlia di vendite
+#1) inserimento dati(): modifico inserimento dati di prima mettendo la chiave gg e inserisco gli importi nel dizionario
+#2) gestione eccezioni
+#3) vendite sopra la media: faccio un get dal dizionario e facco somma e media 
+#4) visualizzo risultati: ciclo for per vedere key e vari importi
